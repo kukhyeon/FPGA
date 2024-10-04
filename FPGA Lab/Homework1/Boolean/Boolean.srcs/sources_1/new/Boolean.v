cdafd1b70cf6f416f5f4c2dbb5b32d8d6a6ae60f
DESKTOP-135RQ3N\namkukhyeon <ska5686a@gmail.com>
2024-10-04 23:19:28 +0900

    주석 새롭게 달기

--- FPGA Lab/Homework1/Boolean/Boolean.srcs/sources_1/new/Boolean.v
+++ FPGA Lab/Homework1/Boolean/Boolean.srcs/sources_1/new/Boolean.v
@@ -1,9 +1,15 @@
-module Boolean(
-    input A,   
-    input B,   
-    input C,  
-    output Y   
-);
-    assign Y = (A & B) | C;
+`timescale 1ns / 1ps
 
+// 모듈 이름 및 port 선언
+module Boolean(
+    input wire A,   
+    input wire B,  
+    input wire C,   
+    output wire Y  
+);
+    
+    wire temp, temp2;               // 임시 wire 선언
+    assign temp = A & ~B & ~C;      // temp는 A가 1이고 B와 C가 0일 때 1
+    assign temp2 = B & C;           // temp2는 B와 C가 모두 1일 때 1
+    assign Y = temp | temp2;        // Y는 temp와 temp2 중 하나라도 1이면 1
 endmodule
