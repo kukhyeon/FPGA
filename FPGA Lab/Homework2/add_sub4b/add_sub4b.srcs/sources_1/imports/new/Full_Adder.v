e7abec28d0c8aec26902151e2bfb75d371510387
DESKTOP-135RQ3N\namkukhyeon <ska5686a@gmail.com>
2024-10-05 22:17:55 +0900

    이전 코드 삭제. 오류가 있던 파일임

--- FPGA Lab/Homework2/add_sub4b/add_sub4b.srcs/sources_1/imports/new/Full_Adder.v
+++
@@ -1,9 +0,0 @@
-module Full_Adder(
-    input a, b, cin,     // 입력: 두 개의 비트 (a, b)와 캐리 입력 (cin)
-    output sum, cout     // 출력: 합 (sum)과 캐리 출력 (cout)
-    );
-
-    // 합 계산 및 캐리 출력 계산
-    assign sum = a ^ b ^ cin;      // 합 계산 (XOR)
-    assign cout = (a & b) | (b & cin) | (a & cin);  // 캐리 출력 계산
-endmodule
