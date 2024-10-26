29c5bac3698c85263e529c764df982d9266a918a
DESKTOP-135RQ3N\namkukhyeon <ska5686a@gmail.com>
2024-10-26 17:35:23 +0900

    한글로 주석 달았는데.. 다 깨져 보인다

--- FPGA Lab/week4/mux_16to1v1.v
+++ FPGA Lab/week4/mux_16to1v1.v
@@ -1,22 +1,23 @@
 `timescale 1ns / 1ps
 
 module mux_16to1v1(
-    input   [3:0]   A,
-    input   [3:0]   B,
-    input   [3:0]   C,
-    input   [3:0]   D,
-    input   [3:0]   sel,
-    output          Y
+    input   [3:0]   A,      // 입력 A
+    input   [3:0]   B,      // 입력 B
+    input   [3:0]   C,      // 입력 C
+    input   [3:0]   D,      // 입력 D
+    input   [3:0]   sel,    // 선택 신호
+    output          Y       // 출력 Y
     );
 
-    wire out_A, out_B, out_C, out_D;
-    
+    wire out_A, out_B, out_C, out_D; // 중간 출력 신호
+
+    // 각 입력(A, B, C, D)의 4비트를 4:1 멀티플렉서로 압축
     mux_4to1 mux_A (.a(A[0]), .b(A[1]), .c(A[2]), .d(A[3]), .sel(sel[1:0]), .out(out_A));
     mux_4to1 mux_B (.a(B[0]), .b(B[1]), .c(B[2]), .d(B[3]), .sel(sel[1:0]), .out(out_B));
     mux_4to1 mux_C (.a(C[0]), .b(C[1]), .c(C[2]), .d(C[3]), .sel(sel[1:0]), .out(out_C));
     mux_4to1 mux_D (.a(D[0]), .b(D[1]), .c(D[2]), .d(D[3]), .sel(sel[1:0]), .out(out_D));
 
+    // 최종 4:1 멀티플렉서를 사용하여 Y로 출력
     mux_4to1 mux_final (.a(out_A), .b(out_B), .c(out_C), .d(out_D), .sel(sel[3:2]), .out(Y));
 
-
-endmodule
+endmodule  
--- FPGA Lab/week4/mux_16to1v2.v
+++ FPGA Lab/week4/mux_16to1v2.v
@@ -1,34 +1,35 @@
 `timescale 1ns / 1ps
-
 module mux_16to1v2(
-    input   [3:0]   A,
-    input   [3:0]   B,
-    input   [3:0]   C,
-    input   [3:0]   D,
-    input   [3:0]   sel,
-    output          Y
+    input   [3:0]   A,      // 입력 A (4비트)
+    input   [3:0]   B,      // 입력 B (4비트)
+    input   [3:0]   C,      // 입력 C (4비트)
+    input   [3:0]   D,      // 입력 D (4비트)
+    input   [3:0]   sel,    // 선택 신호 (4비트)
+    output          Y       // 출력 Y
     );
+    
+    wire out_8to1_1, out_8to1_2; // 8:1 멀티플렉서 출력 신호 선언
 
-    wire out_8to1_1, out_8to1_2;
-
+    // 첫 번째 8:1 멀티플렉서 인스턴스
     mux_8to1 mux_8to1_1 (
-        .a(A[1:0]), 
-        .b(A[3:2]), 
-        .c(B[1:0]), 
-        .d(B[3:2]), 
-        .sel(sel[2:0]), 
-        .out(out_8to1_1)
+        .a(A[1:0]),      // A의 하위 2비트
+        .b(A[3:2]),      // A의 상위 2비트
+        .c(B[1:0]),      // B의 하위 2비트
+        .d(B[3:2]),      // B의 상위 2비트
+        .sel(sel[2:0]),  // 하위 3비트 선택 신호
+        .out(out_8to1_1) // 첫 번째 8:1 MUX 출력
     );
 
+    // 두 번째 8:1 멀티플렉서 인스턴스
     mux_8to1 mux_8to1_2 (
-        .a(C[1:0]), 
-        .b(C[3:2]), 
-        .c(D[1:0]), 
-        .d(D[3:2]), 
-        .sel(sel[2:0]), 
-        .out(out_8to1_2)
+        .a(C[1:0]),      // C의 하위 2비트
+        .b(C[3:2]),      // C의 상위 2비트
+        .c(D[1:0]),      // D의 하위 2비트
+        .d(D[3:2]),      // D의 상위 2비트
+        .sel(sel[2:0]),  // 하위 3비트 선택 신호
+        .out(out_8to1_2) // 두 번째 8:1 MUX 출력
     );
 
+    // 최종 출력 선택: sel[3] 비트에 따라 두 8:1 MUX 중 하나를 선택
     assign Y = (sel[3] == 0) ? out_8to1_1 : out_8to1_2;
-
 endmodule
